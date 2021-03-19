3cfc572d15dd8dec943a517a60999d9297295877
gavinlevy <41343860+gavinlevy@users.noreply.github.com>
2021-03-19 12:58:46 -0400

    Add files via upload

---
+++ CreditProject.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(tidyr)
+
+training <- read_csv("https://raw.githubusercontent.com/mattmcd71/fnce5352_spring2021/main/Assignments/ConsumerCredit/ConsumerCred-test.csv")
+View(training) #view the csv file
